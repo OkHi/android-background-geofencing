052c1e07622aebe7462dbe2b90c9d28fcbd684a2
Julius Kiano <jgkiano@gmail.com>
2020-07-20 02:16:13 +0300

    use new api

--- app/src/main/java/io/okhi/okhiandroidbackgroundgeofencing/MainActivity.java
+++ app/src/main/java/io/okhi/okhiandroidbackgroundgeofencing/MainActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import io.okhi.android_background_geofencing.interfaces.RequestHandler;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingLocationService;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingPermissionService;
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingPlayService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
     BackgroundGeofencingLocationService locationService;
 
+    BackgroundGeofencingPlayService playService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         permissionService = new BackgroundGeofencingPermissionService(this);
         locationService = new BackgroundGeofencingLocationService(this);
+        playService = new BackgroundGeofencingPlayService(this);
+        if (!BackgroundGeofencingPlayService.isGooglePlayServicesAvailable(getApplicationContext())) {
+            playService.requestEnableGooglePlayServices(new RequestHandler() {
+                @Override
+                public void onSuccess() {
+                    Log.v("KIANO", "Play services enabled");
+                }
+
+                @Override
+                public void onError() {
+                    Log.v("KIANO", "Play services disabled");
+                }
+            });
+        }
 //        BackgroundGeofencingLocationService.openLocationServicesSettings(this);
     }
 
@@ -37,5 +54,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         locationService.onActivityResult(requestCode, resultCode, data);
+        playService.onActivityResult(requestCode, resultCode, data);
     }
 }

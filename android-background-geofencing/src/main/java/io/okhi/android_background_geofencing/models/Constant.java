6abaac995fe9d5fdec7c9cb09db95b732a562254
Julius Kiano <jgkiano@gmail.com>
2020-07-20 01:33:29 +0300

    added constants

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
@@ -1,6 +1,8 @@
 package io.okhi.android_background_geofencing.models;
 
 public class Constant {
+    public static final int ENABLE_LOCATION_SERVICES_REQUEST_CODE = 2;
+    public static final long SERVICE_WAIT_DELAY = 2000;
     public static int LOCATION_PERMISSION_REQUEST_CODE = 1;
     public static String PERMISSION_DIALOG_POSITIVE_BUTTON_TEXT = "GRANT";
     public static String PERMISSION_DIALOG_NEGATIVE_BUTTON_TEXT = "CANCEL";

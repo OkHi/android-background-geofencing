c4569cb7f0d4b4359a0cf8d5641c302fcddbacc9
Julius Kiano <jgkiano@gmail.com>
2020-07-20 02:16:06 +0300

    added constant

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
@@ -5,6 +5,7 @@ public class Constant {
     public static int LOCATION_PERMISSION_REQUEST_CODE = 1;
     public static final int ENABLE_LOCATION_SERVICES_REQUEST_CODE = 2;
     public static int OPEN_LOCATION_SERVICES_SETTINGS_REQUEST_CODE = 3;
+    public static final int ENABLE_GOOGLE_PLAY_SERVICES_REQUEST_CODE = 4;
 
     // timers and delays
     public static final long SERVICE_WAIT_DELAY = 2000;

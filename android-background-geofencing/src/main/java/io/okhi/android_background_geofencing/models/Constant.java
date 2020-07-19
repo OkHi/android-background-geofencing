6071b063f02f4519a15694df9f6205d78ded83b8
Julius Kiano <jgkiano@gmail.com>
2020-07-20 01:50:25 +0300

    cleaned up constants

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
@@ -1,9 +1,15 @@
 package io.okhi.android_background_geofencing.models;
 
 public class Constant {
-    public static final int ENABLE_LOCATION_SERVICES_REQUEST_CODE = 2;
-    public static final long SERVICE_WAIT_DELAY = 2000;
+    // request codes
     public static int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    public static final int ENABLE_LOCATION_SERVICES_REQUEST_CODE = 2;
+    public static int OPEN_LOCATION_SERVICES_SETTINGS_REQUEST_CODE = 3;
+
+    // timers and delays
+    public static final long SERVICE_WAIT_DELAY = 2000;
+
+    // dialogs
     public static String PERMISSION_DIALOG_POSITIVE_BUTTON_TEXT = "GRANT";
     public static String PERMISSION_DIALOG_NEGATIVE_BUTTON_TEXT = "CANCEL";
 }

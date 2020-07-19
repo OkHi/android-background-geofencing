f74b7453fbbfe8e5e2d663bd7e83f1e716ac65c6
Julius Kiano <jgkiano@gmail.com>
2020-07-20 01:10:21 +0300

    created AndroidBackgroundGeofencingActivity

---
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/activities/AndroidBackgroundGeofencingActivity.java
@@ -0,0 +1,8 @@
+package io.okhi.android_background_geofencing.activities;
+
+import android.app.Activity;
+
+import androidx.core.app.ActivityCompat;
+
+public class AndroidBackgroundGeofencingActivity extends Activity implements ActivityCompat.OnRequestPermissionsResultCallback {
+}

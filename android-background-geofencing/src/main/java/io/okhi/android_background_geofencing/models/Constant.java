b49b6898670b259b54cb7ce94c46a782500d5bfd
Julius Kiano <jgkiano@gmail.com>
2020-07-20 01:09:07 +0300

    created Constant class

---
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
@@ -0,0 +1,7 @@
+package io.okhi.android_background_geofencing.models;
+
+public class Constant {
+    public static int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    public static String PERMISSION_DIALOG_POSITIVE_BUTTON_TEXT = "GRANT";
+    public static String PERMISSION_DIALOG_NEGATIVE_BUTTON_TEXT = "CANCEL";
+}

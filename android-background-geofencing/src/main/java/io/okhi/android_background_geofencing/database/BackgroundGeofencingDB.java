18a51d37df28927fa8022cb666c5c21563c81efd
Julius Kiano <jgkiano@gmail.com>
2020-07-20 03:43:58 +0300

    created BackgroundGeofencingDB

---
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/database/BackgroundGeofencingDB.java
@@ -0,0 +1,48 @@
+package io.okhi.android_background_geofencing.database;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.snappydb.DB;
+import com.snappydb.DBFactory;
+
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingWebHook;
+import io.okhi.android_background_geofencing.models.Constant;
+
+public class BackgroundGeofencingDB {
+
+    private static String TAG = "BackgroundGeofencingDB";
+
+    private static void save(String key, Object object, Context context) {
+        try {
+            DB db = DBFactory.open(context, Constant.DB_NAME);
+            db.put(key, object);
+            db.close();
+            Log.v(TAG, key + ": successfully saved");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Object get(String key, Class objectClass, Context context) {
+        try {
+            DB db = DBFactory.open(context, Constant.DB_NAME);
+            Object value = db.get(key, objectClass);
+            db.close();
+            Log.v(TAG, key + ": successfully saved");
+            return value;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void saveWebHook(BackgroundGeofencingWebHook webHook, Context context) {
+        save(Constant.DB_WEBHOOK_CONFIGURATION_KEY, webHook, context);
+    }
+
+    public static BackgroundGeofencingWebHook getWebHook(Context context) {
+        return (BackgroundGeofencingWebHook) get(Constant.DB_WEBHOOK_CONFIGURATION_KEY, BackgroundGeofencingWebHook.class, context);
+    }
+
+}

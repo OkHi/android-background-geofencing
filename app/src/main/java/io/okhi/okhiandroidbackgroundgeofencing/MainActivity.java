cd1be0ad4bedeaaf02eb84999ca9053c3520d91a
Julius Kiano <jgkiano@gmail.com>
2020-07-20 05:32:20 +0300

    use geofence.start

--- app/src/main/java/io/okhi/okhiandroidbackgroundgeofencing/MainActivity.java
+++ app/src/main/java/io/okhi/okhiandroidbackgroundgeofencing/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import io.okhi.android_background_geofencing.interfaces.RequestHandler;
+import io.okhi.android_background_geofencing.models.BackgroundGeofence;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingLocationService;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingPermissionService;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingPlayService;
@@ -32,6 +33,32 @@ public class MainActivity extends AppCompatActivity {
         playService = new BackgroundGeofencingPlayService(this);
         webHook = new BackgroundGeofencingWebHook("https://google.com");
         webHook.save(this);
+        permissionService.requestLocationPermission("We need permission", "Pretty please", new RequestHandler() {
+            @Override
+            public void onSuccess() {
+                startGeofence();
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+    }
+
+    private void startGeofence() {
+        BackgroundGeofence backgroundGeofence = new BackgroundGeofence.BackgroundGeofenceBuilder("kiano", -1.313456, 36.9887).build();
+        backgroundGeofence.start(getApplicationContext(), new RequestHandler() {
+            @Override
+            public void onSuccess() {
+                Log.v("KIANO", "🕺");
+            }
+
+            @Override
+            public void onError() {
+                Log.v("KIANO", "😭");
+            }
+        });
     }
 
     @Override

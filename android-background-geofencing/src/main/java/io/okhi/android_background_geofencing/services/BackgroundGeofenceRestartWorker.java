3a77a65ea0d1e4caab211d1287a3fe170896cb8f
Julius Kiano <jgkiano@gmail.com>
2020-07-21 20:10:41 +0300

    geofence restart worker logic

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/services/BackgroundGeofenceRestartWorker.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/services/BackgroundGeofenceRestartWorker.java
@@ -14,6 +14,7 @@ import io.okhi.android_background_geofencing.interfaces.RequestHandler;
 import io.okhi.android_background_geofencing.models.BackgroundGeofence;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingLocationService;
 import io.okhi.android_background_geofencing.models.BackgroundGeofencingPermissionService;
+import io.okhi.android_background_geofencing.models.Constant;
 
 public class BackgroundGeofenceRestartWorker extends Worker {
 
@@ -26,27 +27,52 @@ public class BackgroundGeofenceRestartWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        ArrayList<BackgroundGeofence> geofences = BackgroundGeofencingDB.getAllFailingGeofences(getApplicationContext());
-        if (geofences.isEmpty()) {
-            return Result.success();
-        }
+        long lastGeofenceTransitionEventTimestamp = BackgroundGeofencingDB.getLastGeofenceTransitionEventTimestamp(getApplicationContext());
+        boolean isWithinThreshold = lastGeofenceTransitionEventTimestamp < 0 || System.currentTimeMillis() - lastGeofenceTransitionEventTimestamp < Constant.GEOFENCE_TRANSITION_TIME_STAMP_THRESHOLD;
+        ArrayList<BackgroundGeofence> geofences = BackgroundGeofencingDB.getAllGeofences(getApplicationContext());
+        ArrayList<BackgroundGeofence> failedGeofences = new ArrayList<>();
         boolean isLocationServicesEnabled = BackgroundGeofencingLocationService.isLocationServicesEnabled(getApplicationContext());
         boolean isLocationPermissionGranted = BackgroundGeofencingPermissionService.isLocationPermissionGranted(getApplicationContext());
-        if (isLocationServicesEnabled && isLocationPermissionGranted) {
+
+        for (BackgroundGeofence geofence: geofences) {
+            if (geofence.isFailing()) {
+                failedGeofences.add(geofence);
+            }
+        }
+
+        if (isWithinThreshold && failedGeofences.isEmpty()) {
+            return Result.success();
+        }
+
+        if (!isLocationServicesEnabled || !isLocationPermissionGranted) {
+            return Result.retry();
+        }
+
+        if (!isWithinThreshold) {
+            restartGeofences(geofences, getApplicationContext());
+        } else {
+            restartGeofences(failedGeofences, getApplicationContext());
+        }
+
+        return Result.retry();
+    }
+
+    private void restartGeofences(ArrayList<BackgroundGeofence> geofences, final Context context) {
+        if (geofences != null && !geofences.isEmpty()) {
             for(final BackgroundGeofence geofence: geofences) {
-                geofence.restart(getApplicationContext(), new RequestHandler() {
+                geofence.restart(context, new RequestHandler() {
                     @Override
                     public void onSuccess() {
-                        BackgroundGeofence.setIsFailing(geofence.getId(), false, getApplicationContext());
-                        Log.v(TAG, "Successfully restarted geofence: " + geofence.getId());
+                        Log.v(TAG, "Successfully restarted: " + geofence.getId());
+                        BackgroundGeofence.setIsFailing(geofence.getId(), false, context);
                     }
                     @Override
                     public void onError() {
-                        Log.v(TAG, "Failed to restart geofence: " + geofence.getId());
+                        Log.v(TAG, "Failed to restart: " + geofence.getId());
+                        BackgroundGeofence.setIsFailing(geofence.getId(), true, context);
                     }
                 });
             }
         }
-        return Result.retry();
     }
 }

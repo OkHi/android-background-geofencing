42c57e5227c8fbafddc6e78c9ba562c78c968ca9
Julius Kiano <jgkiano@gmail.com>
2020-07-21 11:53:49 +0300

    receiver to schedule work asap!

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/BackgroundGeofenceBroadcastReceiver.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/BackgroundGeofenceBroadcastReceiver.java
@@ -7,56 +7,50 @@ import android.util.Log;
 
 import androidx.work.BackoffPolicy;
 import androidx.work.Constraints;
-import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
 import com.google.android.gms.location.GeofencingEvent;
 
-import java.util.concurrent.TimeUnit;
-
 import io.okhi.android_background_geofencing.models.BackgroundGeofenceTransition;
 import io.okhi.android_background_geofencing.models.Constant;
-import io.okhi.android_background_geofencing.services.BackgroundGeofenceUploadWorker;
+import io.okhi.android_background_geofencing.services.BackgroundGeofenceTransitionUploadWorker;
 
 public class BackgroundGeofenceBroadcastReceiver extends BroadcastReceiver {
+
+    private String TAG = "GeofenceReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.v("Broadcast", "Received a geofence event");
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
         if (geofencingEvent.hasError()) {
-            // TODO: handle failed geofences
-            return;
+            // TODO: implement restart strategy for failed geofences
+        } else {
+            BackgroundGeofenceTransition transition = new BackgroundGeofenceTransition.BackgroundGeofenceTransitionBuilder(geofencingEvent).build();
+            Log.v(TAG, "Received a " + transition.getTransitionEvent() + "geofence event");
+            transition.save(context);
+            scheduleOneTimeUploadWork(context);
         }
-        scheduleOneTimeGeofenceProcessWork(geofencingEvent, context);
     }
 
-    private void scheduleOneTimeGeofenceProcessWork(GeofencingEvent geofencingEvent, Context context) {
-        try {
-            Constraints constraints = new Constraints.Builder()
-                    .setRequiredNetworkType(NetworkType.CONNECTED)
-                    .setRequiresBatteryNotLow(true)
-                    .build();
-            String payload = new BackgroundGeofenceTransition(geofencingEvent).toJSON();
-            Data geofenceProcessWorkData = new Data.Builder()
-                    .putString(Constant.GEOFENCE_TRANSITION_EVENT_JSON_PAYLOAD, payload)
-                    .build();
-            OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(BackgroundGeofenceUploadWorker.class)
-                    .setConstraints(constraints)
-                    .setInputData(geofenceProcessWorkData)
-                    .addTag(Constant.GEOFENCE_TRANSITION_UPLOAD_REQUEST)
-                    .setInitialDelay(Constant.GEOFENCE_UPLOAD_WORK_INITIAL_DELAY, Constant.GEOFENCE_UPLOAD_WORK_INITIAL_DELAY_TIME_UNIT)
-                    .setBackoffCriteria(
-                            BackoffPolicy.LINEAR,
-                            Constant.GEOFENCE_UPLOAD_WORK_BACK_OFF_DELAY,
-                            Constant.GEOFENCE_UPLOAD_WORK_BACK_OFF_DELAY_TIME_UNIT
-                    )
-                    .build();
-            WorkManager.getInstance(context).enqueue(uploadWorkRequest);
-            Log.v("Broadcast", "Geofence one-time work request queued up");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void scheduleOneTimeUploadWork(Context context) {
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresBatteryNotLow(true)
+                .build();
+
+        OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(BackgroundGeofenceTransitionUploadWorker.class)
+                .setConstraints(constraints)
+                .addTag(Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_TAG)
+                .setInitialDelay(Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_DELAY, Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_DELAY_TIME_UNIT)
+                .setBackoffCriteria(
+                        BackoffPolicy.LINEAR,
+                        Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_BACKOFF_DELAY,
+                        Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_BACKOFF_DELAY_TIME_UNIT
+                )
+                .build();
+        WorkManager.getInstance(context).enqueue(uploadWorkRequest);
+        Log.v(TAG, "Geofence one-time work request queued up");
     }
 }

e55d1a926b8cd6b5ee728aacc12fb5fef9294f1b
Julius Kiano <jgkiano@gmail.com>
2020-07-21 19:06:52 +0300

    finished restart worker

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/services/BackgroundGeofenceRestartWorker.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/services/BackgroundGeofenceRestartWorker.java
@@ -1,12 +1,24 @@
 package io.okhi.android_background_geofencing.services;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import java.util.ArrayList;
+
+import io.okhi.android_background_geofencing.database.BackgroundGeofencingDB;
+import io.okhi.android_background_geofencing.interfaces.RequestHandler;
+import io.okhi.android_background_geofencing.models.BackgroundGeofence;
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingLocationService;
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingPermissionService;
+
 public class BackgroundGeofenceRestartWorker extends Worker {
+
+    private static String TAG = "RestartWorker";
+
     public BackgroundGeofenceRestartWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
@@ -14,6 +26,27 @@ public class BackgroundGeofenceRestartWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        return null;
+        ArrayList<BackgroundGeofence> geofences = BackgroundGeofencingDB.getAllFailingGeofences(getApplicationContext());
+        if (geofences.isEmpty()) {
+            return Result.success();
+        }
+        boolean isLocationServicesEnabled = BackgroundGeofencingLocationService.isLocationServicesEnabled(getApplicationContext());
+        boolean isLocationPermissionGranted = BackgroundGeofencingPermissionService.isLocationPermissionGranted(getApplicationContext());
+        if (isLocationServicesEnabled && isLocationPermissionGranted) {
+            for(final BackgroundGeofence geofence: geofences) {
+                geofence.restart(getApplicationContext(), new RequestHandler() {
+                    @Override
+                    public void onSuccess() {
+                        BackgroundGeofence.setIsFailing(geofence.getId(), false, getApplicationContext());
+                        Log.v(TAG, "Successfully restarted geofence: " + geofence.getId());
+                    }
+                    @Override
+                    public void onError() {
+                        Log.v(TAG, "Failed to restart geofence: " + geofence.getId());
+                    }
+                });
+            }
+        }
+        return Result.retry();
     }
 }

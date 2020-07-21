adb06e9538e62656ca48b32a1b6cc39cf89bf896
Julius Kiano <jgkiano@gmail.com>
2020-07-21 20:02:50 +0300

    added threshold

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
@@ -54,6 +54,7 @@ public class Constant {
     public static final TimeUnit GEOFENCE_TRANSITION_UPLOAD_WORK_DELAY_TIME_UNIT = TimeUnit.MINUTES;
     public static final long GEOFENCE_TRANSITION_UPLOAD_WORK_BACKOFF_DELAY = 45;
     public static final TimeUnit GEOFENCE_TRANSITION_UPLOAD_WORK_BACKOFF_DELAY_TIME_UNIT = TimeUnit.MINUTES;
+    public static final long GEOFENCE_TRANSITION_TIME_STAMP_THRESHOLD = 12 * 60 * 60 * 1000;
     public static int GEOFENCE_TRANSITION_UPLOAD_WORK_MAX_ATTEMPTS = 5;
 
     public static final String GEOFENCE_RESTART_WORK_TAG = "GEOFENCE_RESTART_WORK_TAG";

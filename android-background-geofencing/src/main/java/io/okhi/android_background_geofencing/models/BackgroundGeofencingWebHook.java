4104cd3956bb0769af8e2aec15a857fc9838b72f
Julius Kiano <jgkiano@gmail.com>
2020-07-21 22:45:50 +0300

    change to hash map

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/BackgroundGeofencingWebHook.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/BackgroundGeofencingWebHook.java
@@ -2,8 +2,6 @@ package io.okhi.android_background_geofencing.models;
 
 import android.content.Context;
 
-import org.json.JSONObject;
-
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -16,7 +14,7 @@ public class BackgroundGeofencingWebHook implements Serializable {
     private String url;
     private long timeout = Constant.DEFAULT_WEBHOOK_TIMEOUT;
     private HashMap<String, String> headers;
-    private JSONObject meta;
+    private HashMap<String, Object> meta;
 
     BackgroundGeofencingWebHook() {}
 
@@ -24,18 +22,18 @@ public class BackgroundGeofencingWebHook implements Serializable {
         this.url = url;
     }
 
-    public BackgroundGeofencingWebHook(String url,  int timeout) {
+    public BackgroundGeofencingWebHook(String url, int timeout) {
         this.url = url;
         this.timeout = timeout;
     }
 
-    public BackgroundGeofencingWebHook(String url,  int timeout, HashMap<String, String> headers) {
+    public BackgroundGeofencingWebHook(String url, int timeout, HashMap<String, String> headers) {
         this.url = url;
         this.timeout = timeout;
         this.headers = headers;
     }
 
-    public BackgroundGeofencingWebHook(String url,  int timeout, HashMap<String, String> headers, JSONObject meta) {
+    public BackgroundGeofencingWebHook(String url, int timeout, HashMap<String, String> headers, HashMap<String, Object> meta) {
         this.url = url;
         this.timeout = timeout;
         this.headers = headers;
@@ -65,7 +63,7 @@ public class BackgroundGeofencingWebHook implements Serializable {
         return headerBuilder.build();
     }
 
-    public JSONObject getMeta() {
+    public HashMap<String, Object> getMeta() {
         return meta;
     }
 

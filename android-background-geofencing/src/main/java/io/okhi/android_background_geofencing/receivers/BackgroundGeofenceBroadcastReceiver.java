42cd0433ba9d9e29e3e01047c2ab2a3bf67cdee5
Julius Kiano <jgkiano@gmail.com>
2020-07-21 18:42:09 +0300

    move work to classes

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/BackgroundGeofenceBroadcastReceiver.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/BackgroundGeofenceBroadcastReceiver.java
@@ -3,19 +3,11 @@ package io.okhi.android_background_geofencing.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-
-import androidx.work.BackoffPolicy;
-import androidx.work.Constraints;
-import androidx.work.NetworkType;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkManager;
 
 import com.google.android.gms.location.GeofencingEvent;
 
+import io.okhi.android_background_geofencing.models.BackgroundGeofence;
 import io.okhi.android_background_geofencing.models.BackgroundGeofenceTransition;
-import io.okhi.android_background_geofencing.models.Constant;
-import io.okhi.android_background_geofencing.services.BackgroundGeofenceTransitionUploadWorker;
 
 public class BackgroundGeofenceBroadcastReceiver extends BroadcastReceiver {
 
@@ -25,32 +17,10 @@ public class BackgroundGeofenceBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
         if (geofencingEvent.hasError()) {
-            // TODO: implement restart strategy for failed geofences
+            BackgroundGeofence.setIsFailing(geofencingEvent, true, context);
+            BackgroundGeofence.scheduleGeofenceRestartWork(context);
         } else {
-            BackgroundGeofenceTransition transition = new BackgroundGeofenceTransition.BackgroundGeofenceTransitionBuilder(geofencingEvent).build();
-            Log.v(TAG, "Received a " + transition.getTransitionEvent() + "geofence event");
-            transition.save(context);
-            scheduleOneTimeUploadWork(context);
+            BackgroundGeofenceTransition.scheduleGeofenceTransitionUploadWork(geofencingEvent, context);
         }
-    }
-
-    public void scheduleOneTimeUploadWork(Context context) {
-        Constraints constraints = new Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .setRequiresBatteryNotLow(true)
-                .build();
-
-        OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(BackgroundGeofenceTransitionUploadWorker.class)
-                .setConstraints(constraints)
-                .addTag(Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_TAG)
-                .setInitialDelay(Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_DELAY, Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_DELAY_TIME_UNIT)
-                .setBackoffCriteria(
-                        BackoffPolicy.LINEAR,
-                        Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_BACKOFF_DELAY,
-                        Constant.GEOFENCE_TRANSITION_UPLOAD_WORK_BACKOFF_DELAY_TIME_UNIT
-                )
-                .build();
-        WorkManager.getInstance(context).enqueue(uploadWorkRequest);
-        Log.v(TAG, "Geofence one-time work request queued up");
     }
 }

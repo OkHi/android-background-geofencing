453485c189bbfaa1b7cc8acab11156def49c8a03
Julius Kiano <jgkiano@gmail.com>
2020-07-21 20:10:17 +0300

    restart geofences on device reboot

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/DeviceRebootBroadcastReceiver.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/DeviceRebootBroadcastReceiver.java
@@ -5,12 +5,40 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.ArrayList;
+
+import io.okhi.android_background_geofencing.database.BackgroundGeofencingDB;
+import io.okhi.android_background_geofencing.interfaces.RequestHandler;
+import io.okhi.android_background_geofencing.models.BackgroundGeofence;
+import io.okhi.android_background_geofencing.models.BackgroundGeofenceTransition;
+
 public class DeviceRebootBroadcastReceiver extends BroadcastReceiver {
     public static final String TAG = "DeviceRebootReceiver";
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         Log.v(TAG, "Device reboot detected");
-        // TODO: for devices that receive this, attempt to restart all geofences
+        // TODO: refactor this to a static method
+        ArrayList<BackgroundGeofence> geofences = BackgroundGeofencingDB.getAllGeofences(context);
+        for (final BackgroundGeofence geofence: geofences) {
+            geofence.restart(context, new RequestHandler() {
+                @Override
+                public void onSuccess() {
+                    Log.v(TAG, "Successfully restarted: " + geofence.getId());
+                    BackgroundGeofence.setIsFailing(geofence.getId(), false, context);
+                }
+
+                @Override
+                public void onError() {
+                    Log.v(TAG, "Failed to start: " + geofence.getId());
+                    BackgroundGeofence.setIsFailing(geofence.getId(), true, context);
+                }
+            });
+        }
+        // schedule to restart any failing geofences
+        BackgroundGeofence.scheduleGeofenceRestartWork(context);
+
+        // schedule to upload any stored geofence transitions
+        BackgroundGeofenceTransition.scheduleGeofenceTransitionUploadWork(context);
     }
 }

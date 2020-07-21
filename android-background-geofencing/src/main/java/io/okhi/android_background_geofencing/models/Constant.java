38281446463b0d06076cc32c44a71765ac6d8933
Julius Kiano <jgkiano@gmail.com>
2020-07-21 11:11:31 +0300

    added db constants

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/Constant.java
@@ -22,11 +22,12 @@ public class Constant {
     public static final String PERMISSION_DIALOG_NEGATIVE_BUTTON_TEXT = "CANCEL";
 
     // db
+    // TODO: implement strategy to dump the current db if we bump up the version
     public static final String DB_NAME_VERSION = "v1";
     public static final String DB_NAME = "BACKGROUND_GEOFENCING_DB:" + Constant.DB_NAME_VERSION;
     public static final String DB_WEBHOOK_CONFIGURATION_KEY = "WEBHOOK_CONFIGURATION_KEY";
-    public static final String DB_BACKGROUND_GEOFENCE_STRUCTURE_VERSION = "v1:";
-    public static final String DB_BACKGROUND_GEOFENCE_PREFIX_KEY = "BACKGROUND_GEOFENCE:" + DB_BACKGROUND_GEOFENCE_STRUCTURE_VERSION;
+    public static final String DB_BACKGROUND_GEOFENCE_PREFIX_KEY = "BACKGROUND_GEOFENCE:";
+    public static final String DB_BACKGROUND_GEOFENCE_TRANSITION_PREFIX_KEY = "BACKGROUND_GEOFENCE_TRANSITION:";
 
     // geofence defaults
     public static final long DEFAULT_GEOFENCE_EXPIRATION = Geofence.NEVER_EXPIRE;
@@ -42,6 +43,8 @@ public class Constant {
     public static final String GEOFENCE_TRANSITION_EVENT_JSON_PAYLOAD = "GEOFENCE_TRANSITION_EVENT_JSON_PAYLOAD";
     public static final String GEOFENCE_TRANSITION_UPLOAD_REQUEST = "GEOFENCE_TRANSITION_UPLOAD_REQUEST";
 
+
+
     // TODO: change backoff delay to 45min
     public static int GEOFENCE_UPLOAD_WORK_MAX_ATTEMPTS = 10;
 
@@ -52,4 +55,9 @@ public class Constant {
     public static final long GEOFENCE_UPLOAD_WORK_INITIAL_DELAY = 1;
     public static final TimeUnit GEOFENCE_UPLOAD_WORK_INITIAL_DELAY_TIME_UNIT = TimeUnit.MINUTES;
 
+    // periodic worker settings
+    public static final long GEOFENCE_PERIODIC_WORK_TIME_INTERVAL = 16;
+    public static final TimeUnit GEOFENCE_PERIODIC_WORK_TIME_UNIT = TimeUnit.MINUTES;
+    public static final String GEOFENCE_PERIODIC_WORK_TAG = "GEOFENCE_PERIODIC_WORKER_TAG";
+    public static final String GEOFENCE_PERIODIC_WORK_NAME = "GEOFENCE_PERIODIC_WORKER";
 }

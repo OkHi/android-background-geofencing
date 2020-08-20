d6cedbc1e2fb0d4979f3d79592b4bebfd5dfee0f
Julius Kiano <jgkiano@gmail.com>
2020-08-20 22:44:53 +0300

    created BackgroundGeofencingNotification

---
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/models/BackgroundGeofencingNotification.java
@@ -0,0 +1,59 @@
+package io.okhi.android_background_geofencing.models;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import java.util.Objects;
+
+public class BackgroundGeofencingNotification {
+    private String title;
+    private String text;
+    private int channelImportance;
+    private String channelId;
+    private String channelName;
+    private String channelDescription;
+
+    BackgroundGeofencingNotification() {}
+    
+    BackgroundGeofencingNotification(
+            @NonNull String title,
+            @NonNull String text,
+            @NonNull String channelId,
+            @NonNull String channelName,
+            @NonNull String channelDescription,
+            int channelImportance
+    ) {
+        this.title = title;
+        this.text = text;
+        this.channelId = channelId;
+        this.channelName = channelName;
+        this.channelDescription = channelDescription;
+        this.channelImportance = channelImportance;
+    }
+
+    public Notification getNotification(Context context) {
+        return new NotificationCompat.Builder(context, channelId)
+                .setContentTitle(title)
+                .setContentText(text)
+                .build();
+    }
+
+    public void createNotificationChannel(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    channelId,
+                    channelName,
+                    channelImportance
+            );
+            serviceChannel.setDescription(channelDescription);
+            NotificationManager manager = context.getSystemService(NotificationManager.class);
+            Objects.requireNonNull(manager).createNotificationChannel(serviceChannel);
+        }
+    }
+}

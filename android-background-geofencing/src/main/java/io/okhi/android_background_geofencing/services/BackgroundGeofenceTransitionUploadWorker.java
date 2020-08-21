c6d32f12043afb044ffd2aae88fc2f68348c6c52
Julius Kiano <jgkiano@gmail.com>
2020-08-21 03:15:53 +0300

    defained static uploadTransitions

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/services/BackgroundGeofenceTransitionUploadWorker.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/services/BackgroundGeofenceTransitionUploadWorker.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import org.json.JSONException;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -22,46 +24,55 @@ public class BackgroundGeofenceTransitionUploadWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+        try {
+            boolean result = uploadTransitions(getApplicationContext());
+            if (result) {
+                return Result.success();
+            }
+            return Result.retry();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.failure();
+        }
+    }
+
+    // TODO: is this the best place to put this?
+    public static boolean uploadTransitions(Context context) throws JSONException {
         // get webhook configuration
-        BackgroundGeofencingWebHook webHook = BackgroundGeofencingDB.getWebHook(getApplicationContext());
-
+        BackgroundGeofencingWebHook webHook = BackgroundGeofencingDB.getWebHook(context);
         // get all transitions from db
-        ArrayList<BackgroundGeofenceTransition> transitions = BackgroundGeofencingDB.getAllGeofenceTransitions(getApplicationContext());
-
+        ArrayList<BackgroundGeofenceTransition> transitions = BackgroundGeofencingDB.getAllGeofenceTransitions(context);
         // check if we have a webhook and transitions to upload
         if (webHook == null || transitions.isEmpty()) {
-            return Result.success();
+            return true;
         }
-
         // keep track of upload status
         boolean uploadSuccess = true;
-
         try {
             // attempt to upload all transitions
             for (BackgroundGeofenceTransition transition : transitions) {
                 if (transition.syncUpload(webHook)) {
                     // remove from db if upload succeeds
-                    BackgroundGeofencingDB.removeGeofenceTransition(transition, getApplicationContext());
+                    BackgroundGeofencingDB.removeGeofenceTransition(transition, context);
                 } else {
                     // break if upload process fails
                     uploadSuccess = false;
                     break;
                 }
             }
-
             // check status, attempt retry based on back-off policy
             if (uploadSuccess) {
-                return Result.success();
+                return true;
             } else {
-                return Result.retry();
+                return false;
             }
         } catch (IOException e) {
             e.printStackTrace();
             // retry if we get an IO exception
-            return Result.retry();
+            return false;
         } catch (Exception e) {
             // fail if we have any other exceptions
-            return Result.failure();
+            throw e;
         }
     }
 }

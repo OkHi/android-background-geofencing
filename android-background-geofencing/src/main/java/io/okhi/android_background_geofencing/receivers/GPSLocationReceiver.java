c86e4bb30e9cabda21d583e140e7f1d0f6fb7318
GransonO <oyombegranson@gmail.com>
2022-09-14 11:27:52 +0300

    Check intent action prevent spoofing

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/GPSLocationReceiver.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/GPSLocationReceiver.java
@@ -13,22 +13,22 @@ import io.okhi.android_core.OkHi;
 public class GPSLocationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Boolean isLocationServicesEnabled = OkHi.isLocationServicesEnabled(context);
-        Boolean isForegroundServiceRunning = BackgroundGeofencing.isForegroundServiceRunning(context);
-        Intent locationServicesSettingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-        if (!isLocationServicesEnabled && isForegroundServiceRunning) {
-            int color = Color.argb(255, 255, 0, 0);
-            BackgroundGeofencingNotification.updateNotification(
-              context,
-              "Turn on GPS",
-              "Please turn on GPS to continue with the verification",
-              color,
-              locationServicesSettingsIntent
-            );
-        } else if (isForegroundServiceRunning) {
-            BackgroundGeofencingNotification.resetNotification(context);
-        } else {
-            // Do nothing
+        if(intent.getAction().matches("android.location.PROVIDERS_CHANGED")){
+            boolean isLocationServicesEnabled = OkHi.isLocationServicesEnabled(context);
+            boolean isForegroundServiceRunning = BackgroundGeofencing.isForegroundServiceRunning(context);
+            Intent locationServicesSettingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            if (!isLocationServicesEnabled && isForegroundServiceRunning) {
+                int color = Color.argb(255, 255, 0, 0);
+                BackgroundGeofencingNotification.updateNotification(
+                        context,
+                        "Turn on GPS",
+                        "Please turn on GPS to continue with the verification",
+                        color,
+                        locationServicesSettingsIntent
+                );
+            } else if (isForegroundServiceRunning) {
+                BackgroundGeofencingNotification.resetNotification(context);
+            }  // Do nothing
         }
     }
 }

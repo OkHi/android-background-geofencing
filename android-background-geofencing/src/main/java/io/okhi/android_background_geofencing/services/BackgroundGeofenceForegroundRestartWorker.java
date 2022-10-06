7571eb59147f305b3920cb21c87854bceee6ce58
Julius Kiano <jgkiano@gmail.com>
2022-10-06 09:18:46 +0300

    use canStartForegroundService for restarts

--- android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/services/BackgroundGeofenceForegroundRestartWorker.java
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/services/BackgroundGeofenceForegroundRestartWorker.java
@@ -11,7 +11,9 @@ import io.okhi.android_background_geofencing.BackgroundGeofencing;
 import io.okhi.android_background_geofencing.database.BackgroundGeofencingDB;
 import io.okhi.android_background_geofencing.models.BackgroundGeofenceSetting;
 import io.okhi.android_background_geofencing.models.BackgroundGeofenceUtil;
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingException;
 import io.okhi.android_background_geofencing.models.Constant;
+import io.okhi.android_core.models.OkHiException;
 
 public class BackgroundGeofenceForegroundRestartWorker extends Worker {
     public BackgroundGeofenceForegroundRestartWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
@@ -21,18 +23,24 @@ public class BackgroundGeofenceForegroundRestartWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        BackgroundGeofenceSetting setting = BackgroundGeofencingDB.getBackgroundGeofenceSetting(getApplicationContext());
-        if (setting != null && setting.isWithForegroundService()) {
-            if (!BackgroundGeofencing.isForegroundServiceRunning(getApplicationContext())) {
-                try {
-                    BackgroundGeofencing.startForegroundService(getApplicationContext());
-                } catch (Exception e) {
-                    e.printStackTrace();
+        try {
+            BackgroundGeofenceSetting setting = BackgroundGeofencingDB.getBackgroundGeofenceSetting(getApplicationContext());
+            boolean canStartForegroundService = BackgroundGeofencing.canStartForegroundService(getApplicationContext());
+            if (setting != null && setting.isWithForegroundService() && canStartForegroundService) {
+                if (!BackgroundGeofencing.isForegroundServiceRunning(getApplicationContext())) {
+                    try {
+                        BackgroundGeofencing.startForegroundService(getApplicationContext());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
+            } else {
+                BackgroundGeofenceUtil.cancelForegroundRestartWorker(getApplicationContext());
             }
-        } else {
-            BackgroundGeofenceUtil.cancelForegroundRestartWorker(getApplicationContext());
+        } catch (BackgroundGeofencingException e) {
+            e.printStackTrace();
+        } finally {
+            return Result.success();
         }
-        return Result.success();
     }
 }

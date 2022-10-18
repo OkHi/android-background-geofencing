9581631d0ca1fbaa1bf00b3b2b39f4773d369a5e
Julius Kiano <jgkiano@gmail.com>
2022-10-18 12:13:56 +0300

    created BackgroundGeofenceLocationServicesReceiver

---
+++ android-background-geofencing/src/main/java/io/okhi/android_background_geofencing/receivers/BackgroundGeofenceLocationServicesReceiver.java
@@ -0,0 +1,27 @@
+package io.okhi.android_background_geofencing.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import io.okhi.android_background_geofencing.BackgroundGeofencing;
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingNotification;
+import io.okhi.android_background_geofencing.services.BackgroundGeofencingNotificationService;
+import io.okhi.android_core.OkHi;
+
+public class BackgroundGeofenceLocationServicesReceiver extends BroadcastReceiver {
+  private static String ACTION_MATCH = "android.location.PROVIDERS_CHANGED";
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    if (intent.getAction().matches(ACTION_MATCH) && BackgroundGeofencing.isForegroundServiceRunning(context.getApplicationContext())) {
+      if (!OkHi.isLocationServicesEnabled(context)) {
+        BackgroundGeofencingNotificationService.notifyEnableLocationServices(context);
+      } else if (!OkHi.isBackgroundLocationPermissionGranted(context)) {
+        BackgroundGeofencingNotificationService.notifyEnableBackgroundLocationPermission(context);
+      } else {
+        BackgroundGeofencingNotification.resetNotification(context);
+      }
+    }
+  }
+}

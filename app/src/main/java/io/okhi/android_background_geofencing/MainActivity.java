a239ae23cfb762ddb2de50133f765ab4779846b5
Julius Kiano <jgkiano@gmail.com>
2023-01-23 13:20:47 +0300

    migrated working codebase

--- app/src/main/java/io/okhi/android_background_geofencing/MainActivity.java
+++ app/src/main/java/io/okhi/android_background_geofencing/MainActivity.java
@@ -1,14 +1,291 @@
 package io.okhi.android_background_geofencing;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import io.okhi.android_background_geofencing.BackgroundGeofencing;
+import io.okhi.android_background_geofencing.database.BackgroundGeofencingDB;
+import io.okhi.android_background_geofencing.interfaces.RequestHandler;
+import io.okhi.android_background_geofencing.interfaces.ResultHandler;
+import io.okhi.android_background_geofencing.models.BackgroundGeofence;
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingException;
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingNotification;
+import io.okhi.android_background_geofencing.models.BackgroundGeofencingWebHook;
+import io.okhi.android_background_geofencing.models.WebHookRequest;
+import io.okhi.android_background_geofencing.models.WebHookType;
+import io.okhi.android_core.OkHi;
+import io.okhi.android_core.interfaces.OkHiRequestHandler;
+import io.okhi.android_core.models.OkHiException;
+import io.okhi.android_core.models.OkPreference;
 
 public class MainActivity extends AppCompatActivity {
 
+  OkHi okHi;
+
+  @RequiresApi(api = Build.VERSION_CODES.O)
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    okHi = new OkHi(this);
+    try {
+      OkPreference.setItem("okcollect-launch-payload", "{\"message\":\"verification_status\",\"payload\":{\"style\":{\"base\":{\"color\":\"#ba0c2f\",\"logo\":\"https://cdn.okhi.co/icon.png\"}},\"user\":{\"firstName\":\"Julius\",\"lastName\":\"Kiano\",\"phone\":\"+254700110590\"},\"auth\":{\"authToken\":\"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0IjoibW9iaWxlIiwiYXBwIjp7ImlkIjoieENBTUNpY25TcSIsIm5hbWUiOiJva2hpYXBwIiwiY2xpZW50S2V5IjoiM2RiMTYxN2YtYjI1Yi00YTgwLTgxNjUtODA3N2I0ZDFlYTQ0IiwidXNlciI6eyJpZCI6IkZwSzF0M1FZYngiLCJ1c2VybmFtZSI6Im9raGkifSwib3JnIjp7ImlkIjoiRnBLMXQzUVlieCIsInVzZXJuYW1lIjoib2toaSJ9fSwiYnJhbmNoIjp7ImlkIjoiYldwVndtNjVqeSIsIm5hbWUiOiJkZWZhdWx0In0sInVzZXIiOnsiaWQiOiI1a0NWSTNHNkFPIiwiYWRkcmVzc0NvdW50Ijo5OSwicGhvbmUiOiIrMjU0NzAwMTEwNTkwIn0sImRldmljZUlkIjoiM2M5NGY0ODAtOGY5Ny00ZjYxLTkwNmItNDBkZTllYTEwOTE1Iiwic2NvcGVzIjpbInZlcmlmeSJdLCJpYXQiOjE2NzEwMDI4NTIsImV4cCI6MjYxNzczMDg1Mn0.PvKW8WzJWTiRyHMxeDgssnTxg_8PNOFkV_iEKuCCzmjv-fmNaQNOA0VxlDf920Gafv5fX0PycKABjDgC2SQFwDqF3Sce5pKj36jmVya0yib4iXcgmf1L9rsgA1C1b0JFWam6TnWjy9Spau9gZqrWy-uErVu1B4PHrrzO7mpwL0k\"},\"context\":{\"container\":{\"name\":\"okCollectMobileAndroid\",\"version\":\"version\"},\"developer\":{\"name\":\"okhi\"},\"library\":{\"name\":\"okCollectMobileAndroid\",\"version\":\"version\"},\"platform\":{\"name\":\"android\"},\"permissions\":{\"location\":\"always\"},\"device\":{\"manufacturer\":\"OnePlus\",\"model\":\"HD1900\"},\"locationServicesAvailable\":false},\"config\":{\"streetView\":true,\"protectedApps\":false,\"addressTypes\":{\"work\":true,\"home\":true},\"appBar\":{\"visible\":true,\"color\":\"#ba0c2f\"}},\"locations\":[{\"id\":\"TQm7v9tQGe\"}]},\"url\":\"https://manager-v5.okhi.io\"}", getApplicationContext());
+    } catch (OkHiException e) {
+      e.printStackTrace();
+    }
+
+    BackgroundGeofencingNotification notification = new BackgroundGeofencingNotification(
+      "Yooooooo",
+      "Don't mind us",
+      "OkHi_Channel_id",
+      "OkHi Channel",
+      "My channel description",
+      NotificationManager.IMPORTANCE_HIGH,
+      123,
+      456
+    );
+
+    BackgroundGeofencing.init(this, notification);
+
+    final Button button = findViewById(R.id.location_request);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        okHi.requestLocationPermission("We need permission", "Pretty please", new OkHiRequestHandler<Boolean>() {
+          @Override
+          public void onResult(Boolean result) {
+            okHi.requestBackgroundLocationPermission(new OkHiRequestHandler<Boolean>() {
+              @Override
+              public void onResult(Boolean result) {
+                if (result) {
+                  BackgroundGeofence homeGeofence = new BackgroundGeofence.BackgroundGeofenceBuilder("home1", -1.314611, 36.836299)
+                    .setNotificationResponsiveness(5)
+                    .setLoiteringDelay(60000)
+                    .setInitialTriggerTransitionTypes(0)
+                    .build();
+                  final BackgroundGeofence[] geofences = {homeGeofence};
+                  for (BackgroundGeofence geofence: geofences) {
+                    geofence.start(getApplicationContext(), new RequestHandler() {
+                      @Override
+                      public void onSuccess() {
+                        Log.v("MainActivity", "Work geofence started");
+//                            BackgroundGeofencing.startForegroundService(getApplicationContext());
+                      }
+
+                      @Override
+                      public void onError(BackgroundGeofencingException exception) {
+                        exception.printStackTrace();
+                        Log.v("MainActivity", exception.getMessage());
+                      }
+                    });
+                  }
+                }
+              }
+
+              @Override
+              public void onError(OkHiException exception) {
+
+              }
+            });
+//            if (result) startGeofence();
+          }
+
+          @Override
+          public void onError(OkHiException e) {
+          }
+        });
+      }
+    });
+    final Button local_notification = findViewById(R.id.local_notification);
+    local_notification.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        BackgroundGeofencingNotification notification = new BackgroundGeofencingNotification(
+          "Yo Low",
+          "Am locally triggered",
+          "OkHi_Channel_id",
+          "OkHi Channel",
+          "My channel description",
+          NotificationManager.IMPORTANCE_HIGH,
+          456
+        );
+      }
+    });
+
+    HashMap<String, String> headers = new HashMap<>();
+    headers.put("foo", "bar");
+    JSONObject meta = new JSONObject();
+    try {
+      meta.put("meta", "critic");
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    BackgroundGeofencingWebHook geofenceWebHook = new BackgroundGeofencingWebHook(
+      "https://3439-41-90-189-44.ngrok.io/transits",
+      10000,
+      headers,
+      null,
+      WebHookType.GEOFENCE,
+      WebHookRequest.POST
+    );
+    geofenceWebHook.save(this);
+    BackgroundGeofencingWebHook deviceMetaWebHook = new BackgroundGeofencingWebHook(
+      "https://3439-41-90-189-44.ngrok.io/device-meta",
+      10000,
+      headers,
+      null,
+      WebHookType.DEVICE_PING,
+      WebHookRequest.POST
+    );
+    deviceMetaWebHook.save(this);
+    BackgroundGeofencingWebHook stopVerificationWebHook = new BackgroundGeofencingWebHook(
+      "https://3439-41-90-189-44.ngrok.io/stop/verification",
+      10000,
+      headers,
+      null,
+      WebHookType.STOP,
+      WebHookRequest.PATCH
+    );
+    stopVerificationWebHook.save(this);
+  }
+
+  private void startGeofence() {
+    okHi.requestBackgroundLocationPermission("Hi", "There", new OkHiRequestHandler<Boolean>() {
+      @Override
+      public void onResult(Boolean result) {
+
+      }
+
+      @Override
+      public void onError(OkHiException exception) {
+
+      }
+    });
+
+//        BackgroundGeofence workGeofence = new BackgroundGeofence.BackgroundGeofenceBuilder("work2", -1.313339237582541, 36.842414181487776)
+//                .setNotificationResponsiveness(5)
+//                .setLoiteringDelay(60000)
+//                .setInitialTriggerTransitionTypes(0)
+//                .build();
+//        BackgroundGeofence homeGeofence1 = new BackgroundGeofence.BackgroundGeofenceBuilder("home3", -1.3148501, 36.8363831)
+//                .setNotificationResponsiveness(5)
+//                .setLoiteringDelay(60000)
+//                .setInitialTriggerTransitionTypes(0)
+//                .build();
+//        BackgroundGeofence workGeofence2 = new BackgroundGeofence.BackgroundGeofenceBuilder("work4", -1.313339237582541, 36.842414181487776)
+//                .setNotificationResponsiveness(5)
+//                .setLoiteringDelay(60000)
+//                .setInitialTriggerTransitionTypes(0)
+//                .build();
+//        BackgroundGeofence homeGeofence3 = new BackgroundGeofence.BackgroundGeofenceBuilder("home5", -1.3148501, 36.8363831)
+//                .setNotificationResponsiveness(5)
+//                .setLoiteringDelay(60000)
+//                .setInitialTriggerTransitionTypes(0)
+//                .build();
+//        BackgroundGeofence workGeofence4 = new BackgroundGeofence.BackgroundGeofenceBuilder("work6", -1.313339237582541, 36.842414181487776)
+//                .setNotificationResponsiveness(5)
+//                .setLoiteringDelay(60000)
+//                .setInitialTriggerTransitionTypes(0)
+//                .build();
+
+//        final BackgroundGeofence[] geofences = {homeGeofence, workGeofence, homeGeofence1, workGeofence2, homeGeofence3, workGeofence4};
+
+
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (BackgroundGeofence geofence: geofences) {
+//                    geofence.start(getApplicationContext(), new RequestHandler() {
+//                        @Override
+//                        public void onSuccess() {
+//                            Log.v("MainActivity", "Work geofence started");
+//                        }
+//
+//                        @Override
+//                        public void onError(BackgroundGeofencingException exception) {
+//
+//                        }
+//                    });
+//                }
+//            }
+//        }).start();
+
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        BackgroundGeofencingDB.getAllGeofences(getApplicationContext());
+        Log.v("Thread", "gotten access");
+      }
+    }).start();
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    okHi.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    okHi.onActivityResult(requestCode, resultCode, data);
+  }
+
+  public void isServiceRunning (View view) {
+    showMessage("Service running: " + BackgroundGeofencing.isForegroundServiceRunning(getApplicationContext()));
+  }
+
+  public void startService (View view) {
+    try {
+      BackgroundGeofencing.startForegroundService(getApplicationContext());
+    } catch (BackgroundGeofencingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void stopGeofence(View view) {
+    BackgroundGeofence.stop(getApplicationContext(), "home1", new ResultHandler<String>() {
+      @Override
+      public void onSuccess(String result) {
+        showMessage("Stopped: " + result);
+      }
+
+      @Override
+      public void onError(BackgroundGeofencingException exception) {
+        showMessage("Something went wrong: " + exception.getCode() + "\n" + exception.getMessage());
+      }
+    });
+  }
+
+  public void stopService (View v) {
+    BackgroundGeofencing.stopForegroundService(getApplicationContext());
+  }
+
+  private void showMessage(final String s) {
+    this.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+      }
+    });
   }
 }
